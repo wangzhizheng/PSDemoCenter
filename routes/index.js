e7b155f76d9e7381539cf35434bf9072b833d068
Zhizheng Wang <zhizhengwang@ZhizhengnoMacBook-puro.local>
2018-05-30 20:24:18 +0900

    add mysql and some api for datav

--- routes/index.js
+++ routes/index.js
@@ -60,6 +60,93 @@ router.get('/test', function(req,res){
     res.send('hello world');
 });
 
+router.get('/datav/factory', function (req, res) {
+    //res.sendfile('./downloadpage.html');
+    //var message="Hi, this is a datav message";
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: 'rm-e9b68rq6mmpotwv404o.mysql.japan.rds.aliyuncs.com',
+        user: 'wangzz',
+        password: 'itageEMC0824',
+        database: 'datav'
+    });
+
+    connection.connect();
+
+    var message = [];
+    connection.query('SELECT * FROM factory', function (error, results, fields) {
+        if (error) throw error;
+
+        for (var i = 0; i < results.length; i++) {
+            var k = {
+                "dotid": results[i].dotid,
+                "lat": results[i].lat,
+                "lng": results[i].lng,
+                "value": results[i].value,
+                "info": results[i].info,
+                "type": results[i].type,
+                "name": results[i].name
+            }
+            message[i] = k;
+        }
+        
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify(message));
+        res.end();
+
+    });
+    connection.end();
+
+
+    // var hk={
+    //     "dotid": 0,
+    //     "lat": 43.06417,
+    //     "lng": 141.34694,
+    //     "value": 40,
+    //     "info": "hokkaido",
+    //     "type": "error",
+    //     "name": "hk",
+    //     "rotationAngle": 45
+    //   };
+
+    // var tk={
+    //     "dotid": 1,
+    //     "lat": 35.6811673,
+    //     "lng": 139.7670516,
+    //     "value": 60,
+    //     "info": "Tokyo",
+    //     "type": "ok",
+    //     "name": "tk"
+    //   };
+
+    // var fk={
+    //     "dotid": 2,
+    //     "lat": 33.596302,
+    //     "lng": 130.410784,
+    //     "value": 20,
+    //     "info": "fukuoka",
+    //     "type": "ok",
+    //     "name": "fk"
+    //   };
+
+    // var ok={
+    //     "dotid": 3,
+    //     "lat": 34.702485,
+    //     "lng": 135.495951,
+    //     "value": 20,
+    //     "info": "oosaka",
+    //     "type": "error",
+    //     "name": "ok"
+    //   };
+
+    // var message=[hk,tk,fk,ok];
+
+    // res.setHeader("Content-Type", "application/json");
+    // res.write(JSON.stringify(message));
+    // res.end();
+});
+
+
 router.get('/filelist', function(req,res){
     res.render('../views/filelist.html',{title: 'it is a test title'});
 });
